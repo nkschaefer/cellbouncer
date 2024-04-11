cb87db515e7f37cdd4e6b56816616aa58695a203
Nathan Schaefer <nathan.schaefer@ucsf.edu>
2024-04-11 14:43:57 -0700

    Current progress

--- src/quant_contam.cpp
+++ src/quant_contam.cpp
@@ -43,6 +43,19 @@ void help(int code){
     fprintf(stderr, "===== REQUIRED =====\n");
     fprintf(stderr, "    --output_prefix -o The output prefix used in a prior run of demux_vcf\n");
     fprintf(stderr, "===== OPTIONAL =====\n");
+    fprintf(stderr, "    --ids -i If you limited the individuals to assign when running demux_vcf\n");
+    fprintf(stderr, "       (i.e. your VCF contained extra individuals not in the experiment),\n");
+    fprintf(stderr, "       provide the filtered list of individuals here. Should be a text file\n");
+    fprintf(stderr, "       with one individual name per line, matching individual names in the VCF.\n");
+    fprintf(stderr, "    --ids_doublet -I Similar to --ids/-i argument above, but allows control\n");
+    fprintf(stderr, "       over which doublet identities are considered. Here, you can specify\n");
+    fprintf(stderr, "       individuals and combinations of two individuals to allow. Doublet\n");
+    fprintf(stderr, "       combinations not specified in this file will not be considered.\n");
+    fprintf(stderr, "       Single individuals involved in doublet combinations specified here\n");
+    fprintf(stderr ,"       but not explicitly listed in the file will still be considered.\n");
+    fprintf(stderr, "       Names of individuals must match those in the VCF, and combinations\n");
+    fprintf(stderr, "       of two individuals can be specified by giving both individual names\n");
+    fprintf(stderr, "       separated by \"+\", with names in either order.\n");
     fprintf(stderr, "    --dump_freqs -d After inferring the ambient RNA profile, write a file containing\n");
     fprintf(stderr, "        alt allele frequencies at each type of site in ambient RNA. This file will\n");
     fprintf(stderr, "        be called [output_prefix].contam.dat\n");
@@ -99,6 +112,8 @@ int main(int argc, char *argv[]) {
        {"n_mixprop_trials", required_argument, 0, 'n'},
        {"no_weights", no_argument, 0, 'w'},
        {"dump_freqs", no_argument, 0, 'd'},
+       {"ids", required_argument, 0, 'i'},
+       {"ids_doublet", required_argument, 0, 'I'},
        {0, 0, 0, 0} 
     };
     
@@ -113,6 +128,10 @@ int main(int argc, char *argv[]) {
     int n_mixprop_trials = 10;
     bool weight = true;
     bool dump_freqs = false;
+    string idfile;
+    bool idfile_given = false;
+    string idfile_doublet;
+    bool idfile_doublet_given = false;
 
     int option_index = 0;
     int ch;
@@ -120,7 +139,7 @@ int main(int argc, char *argv[]) {
     if (argc == 1){
         help(0);
     }
-    while((ch = getopt_long(argc, argv, "o:s:e:E:c:l:n:dwph", long_options, &option_index )) != -1){
+    while((ch = getopt_long(argc, argv, "o:s:e:E:c:l:n:i:I:dwph", long_options, &option_index )) != -1){
         switch(ch){
             case 0:
                 // This option set a flag. No need to do anything here.
@@ -130,6 +149,14 @@ int main(int argc, char *argv[]) {
                 break;
             case 'o':
                 output_prefix = optarg;
+                break;
+            case 'i':
+                idfile = optarg;
+                idfile_given = true;
+                break;
+            case 'I':
+                idfile_doublet = optarg;
+                idfile_doublet_given = true;
                 break;
             case 's':
                 inter_species = true;
@@ -177,7 +204,11 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "ERROR: --n_mixprop_trials must be > 0\n");
         exit(1);
     }
-    
+    if (idfile_given && idfile_doublet_given){
+        fprintf(stderr, "ERROR: only one of -i and -I is allowed.\n");
+        exit(1);
+    }
+
     // Load files
     string sample_name = output_prefix + ".samples";
     vector<string> samples;
@@ -191,6 +222,24 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
+    set<int> allowed_ids;
+    set<int> allowed_ids2;
+
+    if (idfile_given){
+        parse_idfile(idfile, samples, allowed_ids, allowed_ids2, true);
+        if (allowed_ids.size() == 0){
+            fprintf(stderr, "No valid individual names found in file %s; allowing \
+all possible individuals\n", idfile.c_str());
+        }
+    }
+    if (idfile_doublet_given){
+        parse_idfile(idfile_doublet, samples, allowed_ids, allowed_ids2, false);
+        if (allowed_ids.size() == 0){
+            fprintf(stderr, "No valid individual names found in file %s; allowing \
+all possible individuals\n", idfile_doublet.c_str());
+        }
+    }
+    
     // Map cell barcodes to numeric IDs of best individual assignments 
     robin_hood::unordered_map<unsigned long, int> assn;
 
@@ -237,7 +286,7 @@ int main(int argc, char *argv[]) {
     string counts_name = output_prefix + ".counts";
     if (file_exists(counts_name)){
         fprintf(stderr, "Loading counts...\n");
-        load_counts_from_file(indv_allelecounts, samples, counts_name); 
+        load_counts_from_file(indv_allelecounts, samples, counts_name, allowed_ids); 
     }
     else{
         fprintf(stderr, "ERROR: no counts found for %s. Please run demux_vcf with same\n",
