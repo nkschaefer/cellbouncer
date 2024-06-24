a4e451229a988c3db5c60c628d7436066d085a1d
Nathan Schaefer <nathan.schaefer@ucsf.edu>
2024-06-24 10:46:02 +0100

    Updated program to export MEX from h5

--- utils/h5tomex.py
+++ utils/h5tomex.py
@@ -7,9 +7,9 @@ import anndata as ad
 import scipy.io
 import pandas as pd
 """
-Given an h5ad format output file containing counts of tags or
-sgRNAas per cell, converts it into MEX format, which can be
-read by demux_tags.
+Given an h5 format (Cellranger or scanpy) output file containing
+counts of tags or sgRNAas per cell, converts it into MEX format, 
+which can be read by demux_tags.
 """
 def parse_args():
     parser = argparse.ArgumentParser(description=__doc__)
@@ -17,6 +17,9 @@ def parse_args():
         required=True)
     parser.add_argument("--out", "-o", help="The directory for output files", \
         required=True)
+    parser.add_argument("--scanpy", "-s", help="Specify input is .h5ad format from \
+AnnData/scanpy. Default is to assume 10X Genomics format h5", \
+        action="store_true", required=False)
     return parser.parse_args()
 
 def main(args):
@@ -27,8 +30,18 @@ def main(args):
         os.mkdir(options.out)
     
     print("Loading {}...".format(options.h5), file=sys.stderr)
-    adata = ad.read_h5ad(options.h5)
-    
+    adata = None
+    try:
+        if not options.scanpy:
+            import scanpy as sc
+            adata = sc.read_10x_h5(options.h5, gex_only=False)
+        else:
+            adata = ad.read_h5ad(options.h5)
+    except:
+        print("ERROR loading input h5 file. Did you specify (or omit) --scanpy correctly?", \
+            file=sys.stderr)
+        exit(1)
+
     print("Writing barcodes...", file=sys.stderr)
     f = gzip.open('{}/barcodes.tsv.gz'.format(options.out), 'wt')
     for bc in adata.obs.index:
@@ -37,8 +50,11 @@ def main(args):
 
     print("Writing features...", file=sys.stderr)
     f = gzip.open('{}/features.tsv.gz'.format(options.out), 'wt')
-    for gene in adata.var.index:
-        print(gene, file=f)
+    if options.tenX:
+        adata.var.to_csv(f, header=False, sep='\t')
+    else:
+        for gene in adata.var.index:
+            print(gene, file=f)
     f.close()
 
     print("Writing mtx...", file=sys.stderr)
